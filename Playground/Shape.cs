f9551a62bcdc6c39f9a4409542f9d4cb2365e4c3
vivek <vivek@athenaeum.in>
2021-05-31 17:58:25 +0530

    done

--- Playground/Shape.cs
+++ Playground/Shape.cs
@@ -6,7 +6,13 @@ namespace Playground
 {
     public class Shape
     {
-        public void Draw()
+        //  When base class is substituted by one of the derived classes
+        //  we still want the derived class' implementation be invoked, should they provide one
+
+        //  In C# all methods are non-overridable by default (sealed by default)
+        //  We explicitly allow the method in the base-class to be overridden in the derived classes
+        //  by making it virtual
+        public virtual void Draw()
         {
             Console.WriteLine("Shape drawing...");
         }
@@ -15,13 +21,19 @@ namespace Playground
     //  Triangle inherits from Shape
     public class Triangle : Shape
     {
-
+        //  This class provides an implementation
+        //  So it uses override in the method signature
+        public override void Draw()
+        {
+            Console.WriteLine("Triangle drawing...");
+        }
     }
 
 
     public class RightTriangle : Triangle
-    {
-
+    {  
+        //  There is no override provided here,
+        //  So, this class will get it's base-class (Triangle) implementation
     }
 
 
