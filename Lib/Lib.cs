cfff7a7556d2b9dbeff74c670a96603b983ccbed
vivek <vivek@athenaeum.in>
2021-05-31 20:24:37 +0530

    done

---
+++ Lib/Lib.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Shape
+    {
+        //  private protected access-modifier is added in C# 7.2
+        //  This member is only accessible to derived classes within the assembly
+        private protected int GetZIndex()
+        {
+            return 10;
+        }
+
+        //  this member is internal for the assembly and protected outside the assembly
+        protected internal void Draw()
+        {
+            Console.WriteLine("Shape drawing...");
+        }
+    }
+
+    //  Sample doesn't inherit from Shape
+    public class Sample
+    {
+        public void DoStuff()
+        {
+            var shape = new Shape();
+            shape.Draw();   //  Draw() is accessible (internal applies here)
+        }
+    }
+
+    public class Square : Shape
+    {
+        public void DrawSquare()
+        {
+            //  GetZIndex() is accessible because Square is a derived class within the assembly
+            Console.WriteLine($"Z-Index of the shape is {base.GetZIndex()}");
+        }
+    }
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lib;
+using System;
 
 namespace Playground
 {
@@ -7,6 +8,17 @@ namespace Playground
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Demo : Shape
+    {
+        public void GetGoing()
+        {
+            //  Only gets access to Draw(); (protected applies here)
+
+            //  Doesn't have the access to GetZIndex() as this is outside the assembly
+            base.Draw();
         }
     }
 }
