314f6e110f97893256537a81b3f471dbb21269ae
vivek <vivek@athenaeum.in>
2021-05-31 17:50:00 +0530

    done

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,17 +6,14 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            //  Interface cannot be instantiated directly
-            //  There must be an implementer on the rvalue
-            IDrivable car = new Vehicle(14.25, "Krishnan");   //  car is an instance of IDrivable
+            Shape s = new Shape();
+            Triangle t = new Triangle();
+            RightTriangle rt = new RightTriangle();
 
-            //  Vehicle-specific methods are not accessible now, even though we have Vehicle implementation
-            //  car.Start();
-            //  car.Stop();
-            //  var owner = car.GetOwnerName();
 
-            car.Drive();    //  Vehicle's implementation will be invoked: polymorphism
-            car.Honk();     //  default implementation will be invoked
+            s.Draw();
+            t.Draw();   //  Triangle inherits from Shape
+            rt.Draw();  //  Right inherits from Triangle
         }
     }
 }
---
+++ Playground/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground
+{
+    public class Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Shape drawing...");
+        }
+    }
+
+    //  Triangle inherits from Shape
+    public class Triangle : Shape
+    {
+
+    }
+
+
+    public class RightTriangle : Triangle
+    {
+
+    }
+
+
+    //  In UML
+
+    //  Shape
+    //    ^
+    //    |
+    //  Triangle
+    //    ^
+    //    |
+    //  RightTriangle
+
+
+}
