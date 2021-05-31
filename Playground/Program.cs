fc612d60c56496e22cf99a2dc4b658d118743620
vivek <vivek@athenaeum.in>
2021-05-31 17:16:16 +0530

    done

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,7 +6,16 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/integral-numeric-types#characteristics-of-the-integral-types
+
+            byte b = 100;       //  unsigned, 8-bits, System.Byte
+            sbyte sb = 100;     //  signed, 8-bits, System.SByte
+            short sh = 125;     //  signed, 16-bits, System.Short
+            ushort ush = 135;   //  unsigned, 16-bits, System.UShort
+            int i = 100;        //  signed, 32-bits, System.Int32
+            uint ui = 100;      //  unsigned, 32-bits, System.UInt32
+            long l = 154;       //  signed, 64-bits, System.Int64
+            ulong ul = 147;     //  unsigned, 64-bits, System.UInt64
         }
     }
 }
