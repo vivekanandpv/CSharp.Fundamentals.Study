c0799f5650d5df3e5b5968aad56ea4edbf10773a
vivek <vivek@athenaeum.in>
2021-05-31 18:21:56 +0530

    done

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -5,13 +5,12 @@ namespace Playground
     class Program
     {
         static void Main(string[] args)
-        { 
-            Shape t = new Triangle();        
-            Shape t2 = new RightTriangle();   
-
-
-            t.Draw();   //  Triangle's implementation
-            t2.Draw();  //  RightTriangle's implemenation
+        {
+            //  Static classes cannot be instantiated, but used directly
+            var square = MathLib.Square(2.0);
+            MathLib.Name = "Math2"; //  possible
+            //  MathLib.Today = new DateTime(2010, 1, 1);   //  not possible (readonly)
+            Console.WriteLine($"Name is: {MathLib.Name}; Year is {MathLib.YEAR}; Square is {square}");   //  accessign const
         }
     }
 }
--- Playground/Shape.cs
+++ Playground/Shape.cs
@@ -4,37 +4,25 @@ using System.Text;
 
 namespace Playground
 {
-    //  abstract classes cannot be directly instantiated
-    //  they are meant to be derived
-    //  like a normal class, it can contain fields, properties, constructors, methods, etc.
-    //  optionally, they may contain abstract members like interfaces
-    //  if there is at least one abstract member, the class must be marked abstract
-    //  but an abstract class need not contain abstract members
-    //  They are used to provide base implementation is complex hierarchies such as GUI frameworks
-    //  Some design patterns such as Template Method, make use of abstract classes
-    public abstract class Shape
+    //  A static class can only contain static or const members
+    //  static classes cannot be inherited, nor can be instantiated
+    //  They are generally used to provide some global functionality and constants
+    //  throughout the application
+    public static class MathLib
     {
-        //  an abstract member should not have a body
-        public abstract void Draw();
-    }
+        //  const members are usually written in all caps, separated by underscores
+        public const int YEAR = 2021;
 
-    //  Triangle inherits from Shape
-    public class Triangle : Shape
-    {
-        //  this must now provide an override
-        public override void Draw()
+        public static string Name = "Math"; //  possible, but bad idea
+
+        //  const can only be applied to compile time constants
+        //  static readonly will make something effectively constant
+        //  Once, assigned, Today cannot be reassigned
+        public static readonly DateTime Today = DateTime.Now;
+
+        public static double Square(double number)
         {
-            Console.WriteLine("Triangle drawing...");
-        }
-    }
-
-
-    public class RightTriangle : Triangle
-    {
-        //  Overriding here is optional, as it doesn't directly inherit from an abstract class
-        public override void Draw()
-        {
-            Console.WriteLine("Right-triangle drawing...");
+            return number * number;
         }
     }
 }
