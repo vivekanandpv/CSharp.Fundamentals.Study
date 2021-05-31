30a47e467b52506810eac582a7029af8a91f0f01
vivek <vivek@athenaeum.in>
2021-05-31 17:53:44 +0530

    done

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,9 +6,12 @@ namespace Playground
     {
         static void Main(string[] args)
         {
+            //  A derived class can substitute itself wherever any of its base-classes (up the hierarchy) are required
+            //  This is polymorphism
+
             Shape s = new Shape();
-            Triangle t = new Triangle();
-            RightTriangle rt = new RightTriangle();
+            Shape t = new Triangle();       //  Triangle is a Shape
+            Shape rt = new RightTriangle(); //  RightTriangle is a Triangle, which is a Shape
 
 
             s.Draw();
