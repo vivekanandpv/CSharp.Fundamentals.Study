0614ae58aea9eb16ddd9c55048ee18818e770838
vivek <vivek@athenaeum.in>
2021-05-31 22:41:58 +0530

    done

---
+++ Playground/Lib.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground
+{
+    //  A struct implicitly inherits from ValueType
+    //  There cannot be explicit inheritance (not even from other structs)
+    //  But structs can implement interfaces
+    //  objects of structs follow value-semantics
+    //  Use structs for light-weight user-defined types where value-semantics is required
+    //  For exmple, complex number, token, etc...
+    public struct Sample : ISample
+    {
+        public string Name { get; set; }
+
+        //  structs can have constructors, but writing a parameter-less constructor is not allowed
+        //  Here in this case, Name must be initialized in the constructor
+        public Sample(string name)
+        {
+            Name = name;
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine($"Struct printing Name: {Name}");
+        }
+    }
+
+    public interface ISample
+    {
+        void PrintMessage();
+    }
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,7 +6,15 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Sample s1 = new Sample("Struct1");
+            Sample s2 = s1; //  full value is copied, not just the reference
+            s1.Name = "Struct1 Name changed";
+            s2.Name = "This is Struct2";
+
+            //  s2 = null;  cannot set null to a struct variable
+
+            Console.WriteLine($"Name of struct1 {s1.Name}");
+            Console.WriteLine($"Name of struct2 {s2.Name}");
         }
     }
 }
