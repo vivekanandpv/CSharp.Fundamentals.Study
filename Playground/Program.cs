fc0ba52c4883ce7cf11651cbe5993b64b312d494
vivek <vivek@athenaeum.in>
2021-05-31 20:42:42 +0530

    done

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,7 +6,17 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Static access to Name: {Sample.Name}");
+            Console.WriteLine($"Const access to SCORE: {Sample.SCORE}");
+
+            //  static method access
+            Sample.PrintMessage();
+
+            //  instance member access
+            var sample = new Sample();
+            Console.WriteLine($"Instance access to Today: {sample.Today}");
+            Console.WriteLine($"Instance access to GetYear: {sample.GetYear()}");
+            Console.WriteLine($"Instance access to GetNameFromInstance: {sample.GetNameFromInstance()}");
         }
     }
 }
---
+++ Playground/Sample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground
+{
+    public class Sample
+    {
+        //  const and static members are accessed from the type, not from the object
+        public const int SCORE = 25;
+
+        public static string Name = "Sample";
+
+        //  Instance member
+        public DateTime Today { get; set; } = DateTime.Now;
+
+
+        //  static methods do not get access to instance members
+        public static void PrintMessage()
+        {
+            Console.WriteLine("Static message...");
+        }
+
+        //  Instance members get access to other instance members as well as static members
+        public int GetYear()
+        {
+            return Today.Year;
+        }
+
+        public string GetNameFromInstance()
+        {
+            return Name;    //  static member access is allowed from instance method
+        }
+    }
+}
