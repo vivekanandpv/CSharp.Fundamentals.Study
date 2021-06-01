3e9af5c0525db08c614b722f14b72418e24e7ba8
vivek <vivek@athenaeum.in>
2021-06-01 11:26:24 +0530

    done

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,15 +6,21 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Sample s1 = new Sample("Struct1");
-            Sample s2 = s1; //  full value is copied, not just the reference
-            s1.Name = "Struct1 Name changed";
-            s2.Name = "This is Struct2";
+            //  structs follow the value semantics
+            //  this means, the variable of a struct holds the complete value
+            var struct1 = new SampleStruct("Struct 1");
+            var struct2 = struct1;  //  the complete value is copied to the new variable
 
-            //  s2 = null;  cannot set null to a struct variable
+            var class1 = new SampleClass("Class 1");
+            var class2 = class1;    //  only the reference to the object is copied
 
-            Console.WriteLine($"Name of struct1 {s1.Name}");
-            Console.WriteLine($"Name of struct2 {s2.Name}");
+            struct1.Name = "Struct 1 name changed"; //  this has no impact on struct2
+            class1.Name = "Class 1 name changed";   //  this impacts class2 as well
+
+            Console.WriteLine($"Struct1: {struct1.Name}");
+            Console.WriteLine($"Struct2: {struct2.Name}");
+            Console.WriteLine($"Class1: {class1.Name}");
+            Console.WriteLine($"Class2: {class2.Name}");
         }
     }
 }
