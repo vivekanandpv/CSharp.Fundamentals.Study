447a1f0eca5bb06d678d5844816c69e0bddf590a
vivek <vivek@athenaeum.in>
2021-06-01 11:44:38 +0530

    done

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,21 +6,30 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            //  structs follow the value semantics
-            //  this means, the variable of a struct holds the complete value
-            var struct1 = new SampleStruct("Struct 1");
-            var struct2 = struct1;  //  the complete value is copied to the new variable
+            var s = new SampleClass("Class Default Name");
+            Console.WriteLine($"Class Before call: {s.Name}");
+            PrintName(s);
+            Console.WriteLine($"Class After call: {s.Name}");
 
-            var class1 = new SampleClass("Class 1");
-            var class2 = class1;    //  only the reference to the object is copied
 
-            struct1.Name = "Struct 1 name changed"; //  this has no impact on struct2
-            class1.Name = "Class 1 name changed";   //  this impacts class2 as well
+            var st = new SampleStruct("Struct Default Name");
+            Console.WriteLine($"Struct Before call: {st.Name}");
+            //  ref parameter must be passed as ref
+            PrintName(ref st);
+            Console.WriteLine($"Struct After call: {st.Name}");
+        }
 
-            Console.WriteLine($"Struct1: {struct1.Name}");
-            Console.WriteLine($"Struct2: {struct2.Name}");
-            Console.WriteLine($"Class1: {class1.Name}");
-            Console.WriteLine($"Class2: {class2.Name}");
+        static void PrintName(SampleClass sample)
+        {
+            Console.WriteLine(sample.Name);
+            sample.Name = "Will this affect s?";
+        }
+
+        //  ref keyword implements the reference symantics for value types
+        static void PrintName(ref SampleStruct sample)
+        {
+            Console.WriteLine(sample.Name);
+            sample.Name = "Will this affect st?";
         }
     }
 }
